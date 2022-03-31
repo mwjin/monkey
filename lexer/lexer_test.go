69cc5314e91843c1ec069f8501569e70df4cee48
Minwoo Jeong <jeongmwj@gmail.com>
2022-03-31 19:02:08 +0900

    Add a test with declare statements

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -30,3 +30,56 @@ func TestNextToken(t *testing.T) {
 		}
 	}
 }
+
+func TestNextTokenDeclareStatement(t *testing.T) {
+	input := `let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+`
+	tests := []token.Token{
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "five"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.INT, Literal: "5"},
+		{Type: token.SEMICOLON, Literal: ";"},
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "ten"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.INT, Literal: "10"},
+		{Type: token.SEMICOLON, Literal: ";"},
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "add"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.FUNCTION, Literal: "fn"},
+		{Type: token.LPAREN, Literal: "("},
+		{Type: token.IDENT, Literal: "x"},
+		{Type: token.COMMA, Literal: ","},
+		{Type: token.IDENT, Literal: "y"},
+		{Type: token.RPAREN, Literal: ")"},
+		{Type: token.LBRACE, Literal: "{"},
+		{Type: token.IDENT, Literal: "x"},
+		{Type: token.PLUS, Literal: "+"},
+		{Type: token.IDENT, Literal: "y"},
+		{Type: token.SEMICOLON, Literal: ";"},
+		{Type: token.RBRACE, Literal: "}"},
+		{Type: token.SEMICOLON, Literal: ";"},
+		{Type: token.EOF, Literal: ""},
+	}
+
+	l := New(input)
+
+	for i, expectedTok := range tests {
+		tok := l.NextToken()
+		if tok.Type != expectedTok.Type {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, expectedTok.Type, tok.Type)
+		}
+		if tok.Literal != expectedTok.Literal {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, expectedTok.Literal, tok.Literal)
+		}
+	}
+}
