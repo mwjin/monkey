1eda510fb49f73949abe2dfa6bac2d255714b4a0
Minwoo Jeong <jeongmwj@gmail.com>
2022-03-31 16:22:16 +0900

    Create `Lexer`

---
+++ lexer/lexer.go
@@ -0,0 +1,33 @@
+package lexer
+
+import "monkey/token"
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+func NewLexer(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// TODO: Support UNICODE (Multibytes)
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) NextToken() *token.Token {
+	tok := &token.Token{token.TokenType(l.ch), string(l.ch)}
+	l.readChar()
+	return tok
+}
