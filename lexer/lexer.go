14f82cc7b458f7449c8055f7a0cd6ce35c774460
Minwoo Jeong <jeongmwj@gmail.com>
2022-04-01 12:35:29 +0900

    Update `NextToken` to use switch statement

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -27,7 +27,32 @@ func (l *Lexer) readChar() {
 }
 
 func (l *Lexer) NextToken() *token.Token {
-	tok := token.New(token.TokenType(l.ch), string(l.ch))
+	var tok *token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newCharToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newCharToken(token.PLUS, l.ch)
+	case ',':
+		tok = newCharToken(token.COMMA, l.ch)
+	case ';':
+		tok = newCharToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newCharToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newCharToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newCharToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newCharToken(token.RBRACE, l.ch)
+	case 0:
+		tok = newCharToken(token.EOF, l.ch)
+	}
 	l.readChar()
 	return tok
+}
+
+func newCharToken(inType token.TokenType, literal byte) *token.Token {
+	return token.New(inType, string(literal))
 }
