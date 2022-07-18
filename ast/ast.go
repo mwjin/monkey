ac03d0bf0e88db91260b3f09073722726a3d5e0c
Minwoo Jeong <jeongmwj@gmail.com>
2022-07-18 20:34:53 +0900

    Add methods
    - Contructors
    - program.AppendStatement

--- ast/ast.go
+++ ast/ast.go
@@ -20,6 +20,12 @@ type Program struct {
 	statements []Statement
 }
 
+func NewProgram() *Program {
+	p := &Program{}
+	p.statements = []Statement{}
+	return p
+}
+
 func (p *Program) TokenLiteral() string {
 	if len(p.statements) > 0 {
 		return p.statements[0].TokenLiteral()
@@ -32,12 +38,23 @@ func (p *Program) GetStatements() []Statement {
 	return p.statements
 }
 
+func (p *Program) AppendStatement(stmt Statement) {
+	p.statements = append(p.statements, stmt)
+}
+
 type LetStatement struct {
 	token token.Token
 	name  *Identifier
 	value Expression
 }
 
+func NewLetStatement(letToken token.Token, name *Identifier, value Expression) *LetStatement {
+	return &LetStatement{
+		token: letToken,
+		name:  name,
+		value: value,
+	}
+}
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.token.Literal() }
 func (ls *LetStatement) GetIdName() string    { return ls.name.value }
@@ -50,5 +67,8 @@ type Identifier struct {
 	value string
 }
 
+func NewIdentifier(idToken token.Token, value string) *Identifier {
+	return &Identifier{token: idToken, value: value}
+}
 func (id *Identifier) expressionNode()      {}
 func (id *Identifier) TokenLiteral() string { return id.token.Literal() }
