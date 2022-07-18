dd89b48b1ee1625b6a46f1c7cf98e7c1b8bf14fc
Minwoo Jeong <jeongmwj@gmail.com>
2022-07-18 20:36:07 +0900

    Implement `ParseProgram` for `let` statements

--- parser/parser.go
+++ parser/parser.go
@@ -25,5 +25,43 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
+	program := ast.NewProgram()
+	for p.curToken.Type() != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.AppendStatement(stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type() {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	letToken := p.curToken
+	p.nextToken()
+	identifier := p.parseIdentifier()
+	p.nextToken()
+	if p.curToken.Type() != token.ASSIGN {
+		return nil
+	}
+	value := p.parseExpression()
+
+	return ast.NewLetStatement(letToken, identifier, value)
+}
+
+func (p *Parser) parseIdentifier() *ast.Identifier {
+	return ast.NewIdentifier(p.curToken, p.curToken.Literal())
+}
+
+func (p *Parser) parseExpression() ast.Expression {
 	return nil
 }
